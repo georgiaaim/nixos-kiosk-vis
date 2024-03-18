90ed9bd02a1292bd07b66709d17979d30f6a9368
Hunter <hdh20267@uga.edu>
2024-03-18 12:47:22 -0400

    first iot flake commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    kioskBase.url = "github:georgiaaim/nixos-kiosk-base"; # Adjust the URL/path to your flake
+    kioskBase.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, myBaseEnv, ... }: {
+    nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        kioskBase.nixosModules.baseEnvironment
+        ./hardware-configuration.nix # The consumer's specific hardware configuration
+        ({ pkgs, ... }: {
+          # Any additional system-specific configuration
+        })
+      ];
+    };
+  };
+}
+
